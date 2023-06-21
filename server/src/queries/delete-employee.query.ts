87e06ba1a0759b27896e014b309d7749086687c0
x0lotl <danil200319771@gmail.com>
2023-06-21 12:48:50 +0300

    some changes

--- server/src/queries/delete-employee.query.ts
+++ server/src/queries/delete-employee.query.ts
@@ -2,7 +2,7 @@ import { defineQuery } from './define-query'
 
 export const deleteEmployeeQuery = defineQuery<string, void, {}>({
   query: `
-    DELETE FROM "User" WHERE "id_employee" = $1
+    DELETE FROM "User" WHERE "id_employee" = $1;
     DELETE FROM "Employee" WHERE "id_employee" = $1
   `,
   transformResult: () => {}
