4db837d1fdbc04f683ecabe6897b6957e7f540f4
Artem Tarasenko <shabashab.04@gmail.com>
2023-06-21 11:56:19 +0300

    fix(server): delete category by id

--- server/src/routes/api/categories/index.ts
+++ server/src/routes/api/categories/index.ts
@@ -3,5 +3,10 @@ import { createPluginFromConfiguration } from '@helpers/createPluginFromConfigur
 export const prefix = '/categories'
 
 export const plugin = createPluginFromConfiguration({
-  routes: [import('./get'), import('./post'), import('./id.patch')]
+  routes: [
+    import('./get'),
+    import('./post'),
+    import('./id.patch'),
+    import('./id.delete')
+  ]
 })
