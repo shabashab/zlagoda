5e2c8e7c4376ca3f6d4b2748a8267e7d69b7a072
Artem Tarasenko <shabashab.04@gmail.com>
2023-06-21 10:50:00 +0300

    fix(server): upc stats query

--- server/src/queries/find-product-sold-count.query.ts
+++ server/src/queries/find-product-sold-count.query.ts
@@ -8,17 +8,17 @@ export const findProductSoldCountQuery = defineQuery<
   },
   number,
   {
-    total_number: number
+    total_number: string
   }
 >({
   query: `
     SELECT SUM("product_nuber") as total_number
-    FROM "Sale"
-    WHERE "UPC" = $1 AND "print_date" > $2 AND "print_date" < $3
+    FROM "Sale" s JOIN "Check" c ON s."check_number" = c."check_number"
+    WHERE s."UPC" = $1 AND c."print_date" > $2 AND c."print_date" < $3
     GROUP BY "UPC"
   `,
   values: (input) => [input.upc, input.from, input.to],
   transformResult: (result) => {
-    return result.rows[0].total_number
+    return parseInt(result.rows[0].total_number)
   }
 })
